143dd2517e6ba428c3ee1bc75f97d29d5a333f56
naveen924611 <106425617+naveen924611@users.noreply.github.com>
2025-02-27 19:25:20 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output:"export"
+// };
+
+// export default nextConfig;
 const nextConfig = {
-    output:"export"
+  output: "export",
+  trailingSlash: true, // Ensures correct paths for static files
+  images: {
+    unoptimized: true, // Fixes missing images
+  },
 };
 
 export default nextConfig;
